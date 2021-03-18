cd4e3bf58c25c8f2d25fc2a3d941129fdbb27374
wilder vagner gaspar <wilderagner@gmail.com>
2021-03-18 19:57:46 -0300

    Queries and Mass Assignment

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,61 @@ Route::get('/', function () {
 });
 
 Route::get('/model', function(){
-    $products = \App\Product::all();
+    //$products = \App\Product::all();
 
-    return $products;
+    // ACTIVE RECORD //
+
+    //$user = new \App\User();
+    //$user = \App\User::find(1);
+    //$user->name = 'Usuário Teste';
+    //$user->email = 'email@test.com';
+    //$user->password = bcrypt('123456');
+
+    //$user->save();
+
+    //return \App\User::all();
+
+    // FIM ACTIVE RECORD //
+
+    /* ==================================== */
+
+    // QUERIES NO ELOQUENT //
+
+    //return \App\User::all();
+    //return \App\User::where('name' , 'Granville Pfeffer')->get();
+    //return \App\User::first();
+    //return \App\User::paginate(10);
+  
+    // FIM QUERIES NO ELOQUENT //
+
+    /* ==================================== */
+
+
+    // MASS ASIGNMENT & MASS UPDATE //
+
+   /*
+        $user = \App\User::create([
+
+            'name' => 'Wilder',
+            'email' => 'wilderagner@email.com',
+            'password' => bcrypt('123456')
+
+        ]);
+
+        dd($user);  
+    */
+
+    $user = \App\User::find(42);
+    $user->update([
+        'name' => 'Atualizando com Mass update'
+    ]);
+    
+    dd($user);
+    
+    return \App\User::find(42);
+
+    // FIM MASS ASSIGNMENT & MASS UPDATE //
+
+   
 });
 
