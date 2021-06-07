9bb989aa983682651ce67cecc18502054cc79c2f
wilder vagner gaspar <wilderagner@gmail.com>
2021-06-07 18:57:26 -0300

    Styling blade with layout template

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,35 +1,45 @@
-<h1>Criar Loja</h1>
-<form action="/admin/stores/store" method="post">
-    <input type="hidden" name="_token" value="{{csrf_token()}}">
-    <div>
-        <label for="" name="">Loja</label>
-        <input type="text" name="name">
-    </div>
-    <div>
-        <label for="" name="">Descrição</label>
-        <input type="text" name="description">
-    </div>
-    <div>
-        <label for="" name="">Telefone</label>
-        <input type="text" name="phone">
-    </div>
-    <div>
-        <label for="" name="">Celular</label>
-        <input type="text" name="mobile_phone">
-    </div>
-    <div>
-        <label for="" name="">Slug</label>
-        <input type="text" name="slug">
-    </div>
-    <div>
-        <label for="" name="">Usuário</label>
-        <select type="text" name="user">
-            @foreach($users as $user)
-                <option value="{{$user->id}}">{{$user->name}}</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <button type="submit">Criar Loja</button>
-    </div>
-</form>    
+@extends('layouts.app')
+
+@section('content') 
+    <h1>Criar Loja</h1>
+    <form action="/admin/stores/store" method="post">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group">
+            <label for="" name="">Loja</label>
+            <input class="form-control" type="text" name="name">
+        </div>
+
+        <div class="form-group">
+            <label for="" name="">Descrição</label>
+            <input class="form-control" type="text" name="description">
+        </div>
+
+        <div class="form-group">
+            <label for="" name="">Telefone</label>
+            <input class="form-control" type="text" name="phone">
+        </div>
+
+        <div class="form-group">
+            <label for="" name="">Celular</label>
+            <input class="form-control" type="text" name="mobile_phone">
+        </div>
+
+        <div class="form-group">
+            <label for="" name="">Slug</label>
+            <input class="form-control" type="text" name="slug">
+        </div>
+
+        <div class="form-group">
+            <label for="" name="">Usuário</label>
+            <select class="form-control" type="text" name="user">
+                @foreach($users as $user)
+                    <option value="{{$user->id}}">{{$user->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
+            <button class="btn btn-success btn-lg mt-3" type="submit">Criar Loja</button>
+        </div>
+    </form>   
+@endsection
